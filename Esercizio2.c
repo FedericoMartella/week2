1344b8b4362138ff851459ea3637425bc6af26b5
FedericoMartella <109146613+FedericoMartella@users.noreply.github.com>
2022-07-14 04:38:17 -0400

    Add files via upload

---
+++ Esercizio2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(){
+
+float a;   //dichiarazione delle variabili
+float b;
+float media;
+
+
+printf("Digita il primo  numero:\n"); //Comando per l'utente
+
+scanf ("%f",&a);
+
+printf("Digita il secondo numero\n");
+
+scanf("%f",&b);
+
+media = (a+b)/2; //Media Artmetica 
+
+
+printf ("la media è: %f\n", media); //Stampa della media
+}
+
