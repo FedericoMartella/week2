78301c102f35fccbfb60e89b55db380e8f1cc0ea
FedericoMartella <109146613+FedericoMartella@users.noreply.github.com>
2022-07-14 04:14:53 -0400

    Add files via upload

---
+++ Esercizio1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main (){
+
+int numero;
+int decremento;
+int incremento;
+{
+printf("digita un numero intero\n");
+scanf ("%d",& numero);
+
+decremento= numero -1;
+incremento= numero +1 ;
+
+printf ("il decremento è %d\n", decremento);
+printf ("l' incremento è %d", incremento);
+
+
+}
+}
